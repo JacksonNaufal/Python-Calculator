aedad4a65046396d58c216d98ca6ed1111b11438
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-28 17:54:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print ("Area and Circumfrence of a Circle Calculator!")
+
+
+diameter = 6
+quater = 0.25
+pi = 3.1415
+
+
+
+
+num3 = input("Enter your Diameter for Area!: ")
+result = float(quater) * float(pi) * float(num3) * float (num3)
+print(result)
+num4 = input("Enter Diameter for Circumfrence!:")
+result = float(pi) * float(num4)
+print(result)
+
+
+
