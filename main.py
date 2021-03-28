dcd667db45571f81744f8b07dac86423c40457f6
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-28 21:04:41 +0000

    Closed Spaces

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 print ("Area and Circumfrence of a Circle Calculator!" )
-
-
-
 diameter = 6
 quater = 0.25
 pi = 3.1415
-
-
 firstname = input("Enter your first name!:")
 lastname = input("Enter your last name!:")
 print("Hello, " + firstname + ", " + lastname + "!")
-
 num3 = input("Enter your Diameter for Area!: ")
 result = float(quater) * float(pi) * float(num3) * float (num3)
 print("The Area of your Circle is:")
@@ -20,6 +14,3 @@ result = float(pi) * float(num4)
 print("The Circumfrence of your Circle is:")
 print(result)
 print("Thank you for using my Area and Circumfrence of a Circle Calculator " + firstname + "!")
-
-
-
