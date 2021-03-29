4a56ef42706751ac01bf9a65e9eda60454c4f4a9
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-29 16:29:30 +0000

    Python Errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Area and Circumfrence of a Circle Calculator!" )
+print("Area and Circumfrence of a Circle Calculator!")
 diameter = 6
 quater = 0.25
 pi = 3.1415
@@ -6,11 +6,11 @@ firstname = input("Enter your first name!:")
 lastname = input("Enter your last name!:")
 print("Hello, " + firstname + ", " + lastname + "!")
 num3 = input("Enter your Diameter for Area!: ")
-result = float(quater) * float(pi) * float(num3) * float (num3)
+result = float(quater) * float(pi) * float(num3) * float(num3)
 print("The Area of your Circle is:")
 print(result)
 num4 = input("Enter Diameter for Circumfrence!:")
 result = float(pi) * float(num4)
 print("The Circumfrence of your Circle is:")
 print(result)
-print("Thank you for using my Area and Circumfrence of a Circle Calculator " + firstname + "!")
+print("Thank you for using my Area and Circumfrence of a Circle Calculator" + firstname + "!")
