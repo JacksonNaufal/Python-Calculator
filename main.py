c0832f4bb0df94ce9f48f8dab8a36832af12ddf0
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-30 16:24:53 +0000

    Format

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ num3 = input("Enter your Diameter for Area!: ")
 result = float(quater) * float(pi) * float(num3) * float(num3)
 print("The Area of your Circle is:")
 print(result)
-num4 = input("Enter Diameter for Circumfrence!:")
+num4 = input("Enter Diameter for Circumference!:")
 result = float(pi) * float(num4)
 print("The Circumfrence of your Circle is:")
 print(result)
 print(
-  "Thank you for using my Area and Circumfrence of a Circle Calculator "
-   + firstname 
-   + "!"
+   "Thank you for using my Area and Circumference of a Circle Calculator "
+    + firstname 
+    + "!"
 )
